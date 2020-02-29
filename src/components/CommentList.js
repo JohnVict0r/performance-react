28177df979700ce4182078252b169cecf08e790e
John Victor <johnvictorio3@gmail.com>
2020-02-29 14:25:33 -0300

    projeto com problemas de performance

---
+++ src/components/CommentList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import CommentItem from './CommentItem';
+
+function CommentList({ comments }) {
+  return (
+    <div className="comment-list">
+      <strong>Comentários ({comments.length})</strong>
+
+      <ul>
+        {comments.map(comment => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CommentList;
